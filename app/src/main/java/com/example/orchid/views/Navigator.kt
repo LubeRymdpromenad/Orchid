d910d2c9c261e93246c85c35f3c74ec8d3535d2f
Lars Viking <larsviking@mbpsomtlhorlars.lan>
2022-01-03 20:15:58 +0100

    Changed to Jetpack navigation

--- app/src/main/java/com/example/orchid/views/Navigator.kt
+++
@@ -1,8 +0,0 @@
-package com.example.orchid.views
-
-import com.example.orchid.data.PlantData
-
-interface Navigator {
-    fun openDetails(plantData: PlantData)
-    fun searchUnsplash(query: String)
-}
